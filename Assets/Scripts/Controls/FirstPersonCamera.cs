d4a3d618075b4bccc23735fe47c63b6c289f22d2
twistedPan <dedu@gmx.ch>
2022-07-08 22:01:05 +0200

    movement update

--- Assets/Scripts/Controls/FirstPersonCamera.cs
+++ Assets/Scripts/Controls/FirstPersonCamera.cs
@@ -10,10 +10,14 @@ public class FirstPersonCamera : MonoBehaviour
     [Tooltip("Higher value is more sensitive.")]
     [SerializeField] private Vector2 camSensitivity = new Vector2(3,2);
 
+    [SerializeField] private float headBobtAmt = 0.1f;
+    
     private float distance = 0.01f;
     private float currentX = 0.0f;
     private float currentY = 0.0f;
     private float playerHeight;
+    private float headMovement = 0.0f;  
+
 
     private void Start()
     {
@@ -23,7 +27,7 @@ public class FirstPersonCamera : MonoBehaviour
 
     private void LateUpdate() 
     {
-        Vector3 playerHead = new Vector3(lookAt.position.x, (lookAt.position.y-1) + playerHeight, lookAt.position.z);
+        Vector3 playerHead = new Vector3(lookAt.position.x, (lookAt.position.y-1) + playerHeight + headMovement, lookAt.position.z);
         Vector3 dir = new Vector3(0,0,-distance);
 
         //* Clamp Up/Down rotation
@@ -48,4 +52,37 @@ public class FirstPersonCamera : MonoBehaviour
         currentY += v.x * (1/camSensitivity.x);
     }
 
+
+    /// <summary>
+    /// Calculates an up and down movement
+    /// </summary>
+    /// <param name="amount">float - </param>
+    /// <param name="sprinting">bool - </param>
+    public void HeadMove(float amount, bool sprinting) 
+    {
+        amount = Mathf.Clamp(amount, 0,1);
+
+        float stepLerp = MapRange(amount, 0,1, Mathf.PI,Mathf.PI*2);
+        float sinMove = Mathf.Sin(stepLerp); 
+        float headBob = sprinting ? 0.25f : headBobtAmt;
+        float camLerp = MapRange(sinMove, -1,1, -headBob,headBob);
+
+        headMovement = Mathf.Sin(camLerp);
+    }
+
+
+    // Create range from value n which acts in range start1 to stop1 to new range
+    private float MapRange(float n, float start1, float stop1, float start2, float stop2) 
+    {
+        float newval = (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+        //if (newval != ) {return newval;}
+        if (start2 < stop2) 
+        {
+            return Mathf.Clamp(newval, start2, stop2);
+        } 
+        else 
+        {
+            return Mathf.Clamp(newval, stop2, start2);
+        }
+    }
 }
--- Assets/Scripts/Controls/Player_FP.cs
+++ Assets/Scripts/Controls/Player_FP.cs
@@ -1,26 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player_FP : MonoBehaviour
 {   
     [Space][Header("Walk & Sprint")]
-    [SerializeField] private float maxWalkSpeed = 6.0f;
-    [SerializeField] private float walkAcceleration = 6.0f;
-    [SerializeField] private float maxSprintSpeed = 12.0f;
-    [SerializeField] private float sprintAcceleration = 12.0f;
-    [SerializeField] private float speed = 0.0f;
+    [SerializeField] private float maxWalkSpeed = 4.0f;
+    [SerializeField] private float walkAcceleration = 5.0f;
+    [SerializeField] private float maxSprintSpeed = 8.0f;
+    [SerializeField] private float sprintAcceleration = 9.0f;
+    private float speed = 0.0f;
 
     [Space][Header("Stuff")]
     public float playerHeight = 1.8f;
+
+    [Space][Header("Step Cycle")]
+    [SerializeField] private float runstepInterval = 0.6f;
+    [SerializeField] private float stepInterval = 12.0f;
+    private float stepCycle = 0.0f;
+    private float nextStep = 0.0f;
 
     private PlayerController controls;
     private CharacterController controller;
     private Transform camTransform;
     private FirstPersonCamera firstPersonCamera;
     private Vector3 origin;
-    private Vector3 playerVelocity;
+    private float playerDownVelocity;
     public Vector2 moveInput { get; private set; }
 
     private bool isGrounded = true;
@@ -35,21 +40,18 @@ public class Player_FP : MonoBehaviour
         controller = GetComponent<CharacterController>();
 
         origin = transform.position;
-        GetComponent<MeshRenderer>().enabled = false;
 
         //* Set Inputs
         controls = new PlayerController();
         controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
         controls.Player.Move.canceled += _ => moveInput = new Vector2(0,0);
-
         controls.Player.Sprint.performed += _ => isSprinting = true;
         controls.Player.Sprint.canceled += _ => isSprinting = false;
-
         controls.Player.Look.started += ctx => FindObjectOfType<FirstPersonCamera>().Look(ctx.ReadValue<Vector2>());
 
+        //* hide mouse cursor
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-
     }
 
     void Update() 
@@ -65,6 +67,8 @@ public class Player_FP : MonoBehaviour
     {
         Vector3 move = new Vector3(moveInput.x, 0.0f, moveInput.y).normalized;
         Vector3 moveDir = new Vector3(0.0f, 0.0f, 0.0f);
+        
+        isGrounded = IsGrounded();
 
         //* calc movement speed and sprintSpeed
         float maxMovingSpeed = isSprinting ? maxSprintSpeed : maxWalkSpeed;                 //* set maximum speed
@@ -94,11 +98,7 @@ public class Player_FP : MonoBehaviour
         } 
 
         //* 
-        isGrounded = controller.isGrounded;
-        if (isGrounded && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
+        if (isGrounded && playerDownVelocity < 0) playerDownVelocity = 0f;
 
         //* set player rotation
         transform.rotation = Quaternion.Euler(0f, camTransform.eulerAngles.y, 0f);
@@ -107,12 +107,55 @@ public class Player_FP : MonoBehaviour
         controller.Move(moveDir * Time.deltaTime * speed);
 
         //* apply gravity
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        playerDownVelocity += gravityValue * 4 * Time.deltaTime;
+        controller.Move(new Vector3(0,playerDownVelocity,0) * Time.deltaTime);
+
+        //* calc steps
+        StepCycle(speed);
     }
 
 
+    //* Ground Check
+    bool IsGrounded()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, Vector3.down, out hit, controller.height/2+0.1f);
+        return hit.collider != null;
+    }
+
+
+    //* Step Simulation
+    void StepCycle(float speed)
+    {
+        if (moveInput.magnitude > 0) 
+        {
+            stepCycle += (moveInput.magnitude + (speed*(isSprinting ? runstepInterval : 1f))/10)*
+                             Time.fixedDeltaTime;
+        }
+        else if (stepCycle > 1)
+        {
+            stepCycle = 0;
+            nextStep = 0;
+        }
+
+        float stepProgressAmount = (nextStep - stepCycle) / stepInterval;
+        firstPersonCamera.HeadMove(stepProgressAmount, isSprinting);
+        
+        if (!(stepCycle > nextStep)) return;
+        else
+        {
+            nextStep = stepCycle + stepInterval;
+            PlayFootStepSound();
+        }
+    }
+
+    //* Step Sound
+    void PlayFootStepSound()
+    {
+        // Play Footstep Audio
+    }
+
+    
     private void OnEnable() => controls.Enable();
     private void OnDisable() => controls.Disable();
-
 }
